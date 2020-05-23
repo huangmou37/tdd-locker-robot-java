490f170e41b375b9c673a34aff1d15dfb45986ac
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-05-23 15:31:46 +0800

    [Yang Liu & Cheng Huang] #NA update test cases format & naming

--- src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SmartLockerRobotTest.java
@@ -2,8 +2,9 @@ package cn.xpbootcamp.locker_robot;
 
 import cn.xpbootcamp.locker_robot.exception.InvalidReceiptException;
 import cn.xpbootcamp.locker_robot.exception.LockerIsFullException;
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -13,100 +14,120 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class SmartLockerRobotTest {
 
   @Test
-  void should_deposit_in_first_locker_and_return_receipt_when_deposit_given_two_no_full_lockers__and_available_number_of_first_one_is_bigger_than_second() {
+  void should_deposit_in_first_locker_and_return_receipt_when_deposit_given_two_non_full_lockers_and_remaining_of_first_one_is_larger_than_second() {
+    // given
     Locker firstLocker = new Locker(2);
     Locker secondLocker = new Locker(1);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
 
+    // when
     Receipt receipt = smartLockerRobot.deposit(new UserPackage());
 
+    // then
     assertNotNull(receipt);
     assertEquals(firstLocker, smartLockerRobot.findPackageLocation(receipt));
   }
 
   @Test
-  void should_deposit_in_second_locker_and_return_receipt_when_deposit_given_two_no_full_lockers_and_available_number_of_first_one_is_less_than_second() {
+  void should_deposit_in_second_locker_and_return_receipt_when_deposit_given_two_non_full_lockers_and_remaining_of_first_one_is_smaller_than_second() {
+    // given
     Locker firstLocker = new Locker(1);
     Locker secondLocker = new Locker(2);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
 
+    // when
     Receipt receipt = smartLockerRobot.deposit(new UserPackage());
 
+    // then
     assertNotNull(receipt);
     assertEquals(secondLocker, smartLockerRobot.findPackageLocation(receipt));
   }
 
   @Test
-  void should_deposit_in_first_or_second_locker_and_return_receipt_when_deposit_given_three_no_full_lockers_and_available_number_of_first_two_are_both_and_is_more_than_the_second() {
+  void should_deposit_in_first_or_second_locker_and_return_receipt_when_deposit_given_three_non_full_lockers_and_remaining_of_first_two_are_equal_and_larger_than_the_third() {
+    // given
     Locker firstLocker = new Locker(2);
     Locker secondLocker = new Locker(2);
     Locker ThirdLocker = new Locker(1);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
 
+    // when
     Receipt receipt = smartLockerRobot.deposit(new UserPackage());
 
+    // then
     assertNotNull(receipt);
-    assertTrue(smartLockerRobot.findPackageLocation(receipt) == firstLocker || smartLockerRobot.findPackageLocation(receipt) == secondLocker);
+    assertTrue(smartLockerRobot.findPackageLocation(receipt) == firstLocker
+        || smartLockerRobot.findPackageLocation(receipt) == secondLocker);
   }
 
   @Test
-  void should_throw_exception_when_deposit_given_two_lockers_and_available_number_of_both_locker_are_zero() {
-    Locker firstLocker = new Locker(0);
-    Locker secondLocker = new Locker(0);
+  void should_throw_exception_when_deposit_given_two_full_lockers() {
+    // given
+    Locker firstLocker = new Locker(1);
+    Locker secondLocker = new Locker(1);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
+    smartLockerRobot.deposit(new UserPackage());
+    smartLockerRobot.deposit(new UserPackage());
 
+    // when & then
     assertThrows(LockerIsFullException.class, () -> secondLocker.deposit(new UserPackage()));
   }
 
   @Test
   void should_return_deposited_package_when_withdraw_given_valid_receipt_with_package_in_first_locker() {
-
+    // given
     Locker firstLocker = new Locker(2);
     Locker secondLocker = new Locker(1);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
     UserPackage userPackage = new UserPackage();
     Receipt receipt = smartLockerRobot.deposit(userPackage);
 
+    // when
     UserPackage result = smartLockerRobot.withdraw(receipt);
 
+    // then
     assertEquals(userPackage, result);
   }
 
   @Test
   void should_return_deposited_package_when_withdraw_given_valid_receipt_with_package_in_second_locker() {
-
+    // given
     Locker firstLocker = new Locker(1);
     Locker secondLocker = new Locker(2);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
     UserPackage userPackage = new UserPackage();
     Receipt receipt = smartLockerRobot.deposit(userPackage);
 
+    // when
     UserPackage result = smartLockerRobot.withdraw(receipt);
 
+    // then
     assertEquals(userPackage, result);
   }
 
   @Test
   void should_throw_exception_when_withdraw_given_invalid_receipt() {
-
+    // given
     Locker firstLocker = new Locker(1);
     Locker secondLocker = new Locker(2);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
 
+    // when & then
     assertThrows(InvalidReceiptException.class, () -> smartLockerRobot.withdraw(new Receipt()));
   }
 
   @Test
   void should_throw_exception_when_withdraw_given_used_receipt() {
-
+    // given
     Locker firstLocker = new Locker(1);
     Locker secondLocker = new Locker(2);
     SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(firstLocker, secondLocker));
     Receipt deposit = smartLockerRobot.deposit(new UserPackage());
+
+    // when
     smartLockerRobot.withdraw(deposit);
 
+    // then
     assertThrows(InvalidReceiptException.class, () -> smartLockerRobot.withdraw(deposit));
   }
-
-
 }
