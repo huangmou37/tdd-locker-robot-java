a03b2df616013fdfe4ed51d801f098719204f1da
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-05-30 17:05:13 +0800

    [Yang Liu & Cheng Huang] #NA add test case for depositing package given multiple lockers has the highest vacancy rate

--- src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/SuperLockerRobotTest.java
@@ -1,11 +1,13 @@
 package cn.xpbootcamp.locker_robot;
 
+import cn.xpbootcamp.locker_robot.exception.InvalidReceiptException;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class SuperLockerRobotTest {
 
@@ -78,4 +80,25 @@ class SuperLockerRobotTest {
     assertNotNull(receipt);
     assertEquals(depositedPackage, secondLocker.withdraw(receipt));
   }
+
+  @Test
+  void should_deposit_in_first_or_second_locker_and_return_receipt_when_deposit_given_three_non_full_lockers_and_vacancy_rate_of_first_two_are_equal_and_larger_than_the_third() {
+    // given
+    Locker firstLocker = new Locker(3);
+    Locker secondLocker = new Locker(3);
+    Locker thirdLocker = new Locker(2);
+    SuperLockerRobot superLockerRobot = new SuperLockerRobot(Arrays.asList(firstLocker, secondLocker, thirdLocker));
+    firstLocker.deposit(new UserPackage());
+    secondLocker.deposit(new UserPackage());
+    thirdLocker.deposit(new UserPackage());
+
+    // when
+    Receipt receipt = superLockerRobot.deposit(new UserPackage());
+
+    // then
+    assertNotNull(receipt);
+    assertThrows(InvalidReceiptException.class, () -> thirdLocker.withdraw(receipt));
+  }
+
+
 }
